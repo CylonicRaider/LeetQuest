992f8840328279188ff031433c42e9c49496f70c
CylonicRaider <CylonicRaider@users.noreply.github.com>
2021-07-25 18:36:18 +0200

    Fix build script bug

--- bin/build-client.js
+++ bin/build-client.js
@@ -7,27 +7,23 @@ var BUILD_DIR = path.join(__dirname, '../client-build');
 var PROJECT_DIR = path.join(__dirname, '../client/js');
 
 function adjustPath(filepath, definition) {
-  if (typeof definition == 'string') {
-    if (definition == 'remove') {
-      fs.rmSync(filepath, {recursive: true});
+    if (typeof definition === 'function') {
+        definition = definition(filepath);
     }
-  } else if (typeof definition == 'object') {
-    if (!fs.lstatSync(filepath).isDirectory())
-      throw new Error('Expected directory at ' + filepath);
-    if (Array.isArray(definition)) {
-      var defObject = {'': 'remove'};
-      definition.forEach(function(name) {
-        defObject[name] = 'keep';
-      });
-      definition = defObject;
+    if (typeof definition === 'string') {
+        if (definition === 'remove') {
+            fs.rmSync(filepath, {recursive: true});
+        }
+    } else if (typeof definition === 'object') {
+        if (!fs.lstatSync(filepath).isDirectory())
+            throw new Error('Expected directory at ' + filepath);
+        fs.readdirSync(filepath).forEach(function(name) {
+            var action = definition[name] || definition[''] || 'keep';
+            adjustPath(path.join(filepath, name), action);
+        });
+    } else {
+        throw new Error('Invalid path adjustment definition: ' + definition);
     }
-    fs.readdirSync(filepath).forEach(function(name) {
-      var action = definition[name] || definition[''] || 'keep';
-      adjustPath(path.join(filepath, name), action);
-    });
-  } else {
-    throw new Error('Invalid path adjustment definition: ' + definition);
-  }
 }
 
 console.log('Cleaning up build directory...');
@@ -39,11 +35,23 @@ child_process.spawnSync('node', ['../../bin/r.js', '-o', 'build.js'],
 
 console.log('Removing unnecessary files...');
 adjustPath(BUILD_DIR, {
-  'js': ['game.js', 'home.js', 'log.js', 'require-jquery.js', 'modernizr.js',
-         'css3-mediaqueries.js', 'mapworker.js', 'detect.js',
-         'underscore.min.js', 'text.js'],
-  'sprites': 'remove',
-  'config': 'remove'
+    'js': {
+        'game.js': 'keep',
+        'home.js': 'keep',
+        'log.js': 'keep',
+        'require-jquery.js': 'keep',
+        'modernizr.js': 'keep',
+        'css3-mediaqueries.js': 'keep',
+        'mapworker.js': 'keep',
+        'detect.js': 'keep',
+        'underscore.min.js': 'keep',
+        'text.js': 'keep',
+        '': function(filepath) {
+            return (fs.lstatSync(filepath).isFile()) ? 'remove' : 'keep';
+        }
+    },
+    'sprites': 'remove',
+    'config': 'remove'
 });
 
 console.log('Done.');
