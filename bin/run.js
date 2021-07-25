8279a164e0f7e7087b6328ba3354918a963a5a1c
CylonicRaider <CylonicRaider@users.noreply.github.com>
2021-07-25 23:47:38 +0200

    Add driver script

---
+++ bin/run.js
@@ -0,0 +1,36 @@
+
+var fs = require('fs');
+var path = require('path');
+var process = require('process');
+var child_process = require('child_process');
+
+var minimist = require('minimist');
+
+var SERVER_DIR = path.join(__dirname, '../server');
+
+function mergeSettings(base, merge) {
+    Object.keys(merge).forEach(function(k) {
+        if (merge[k] === undefined) return;
+        base[k] = merge[k];
+    });
+    return base;
+}
+
+function main() {
+    var args = minimist(process.argv.slice(2));
+    var settings = {
+        port: args.port,
+        nb_worlds: args['world-count'],
+        nb_players_per_world: args['world-size'],
+        debug_level: args['log-level']
+    };
+    var defaults = JSON.parse(fs.readFileSync(path.join(SERVER_DIR,
+                                                        'config.json')));
+    settings = mergeSettings(defaults, settings);
+    fs.writeFileSync(path.join(SERVER_DIR, 'config_local.json'),
+                     JSON.stringify(settings, null, 4) + '\n');
+    child_process.spawnSync('node', [path.join(SERVER_DIR, 'js/main.js')],
+                            {cwd: path.join(SERVER_DIR, '..')});
+}
+
+main();
