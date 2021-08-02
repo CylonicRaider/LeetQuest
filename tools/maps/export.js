2921d51f05ce13bb857a075a1b65771713573cf0
CylonicRaider <CylonicRaider@users.noreply.github.com>
2021-08-02 23:58:11 +0200

    Commence porting the map exporter to pure JS
    The final Growl notification is deliberately excluded.

---
+++ tools/maps/export.js
@@ -0,0 +1,27 @@
+
+var fs = require('fs');
+var path = require('path');
+var process = require('process');
+var child_process = require('child_process');
+
+var SRC_FILE = 'tmx/map.tmx';
+var TEMP_FILE = 'tmx/map.tmx.json';
+
+function runCommand(cmdline) {
+    var result = child_process.spawnSync(cmdline[0], cmdline.slice(1),
+        {cwd: __dirname, stdio: 'inherit'});
+    if (result.status !== 0) process.exit(1);
+}
+
+var mode = process.argv[2] || 'client';
+var DEST_FILE = (mode === 'client') ?
+    '../../client/maps/world_client' :
+    '../../server/maps/world_server.json';
+
+runCommand(['python2', 'tmx2json.py', SRC_FILE, TEMP_FILE]);
+
+runCommand(['node', 'exportmap.js', TEMP_FILE, DEST_FILE, mode]);
+
+fs.unlinkSync(TEMP_FILE);
+
+console.log('Done.');
