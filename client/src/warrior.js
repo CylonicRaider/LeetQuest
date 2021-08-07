00d5e312370d36e1896cef408010ad2bee301b0f
Hans Christian Schmitz <hcsch@hcsch.eu>
2021-08-07 18:47:08 +0200

    Fix client side warrior attack issue
    When a client's own player (i.e. the warrior) attacked an enemy,
    the player doesn't get added as an attacker to the attacked entity,
    because the players attack logic is handled separately. When an entity
    died the player however still got removed as an attacker with the
    regular logic, resulting in an attacker being removed that wasn't there
    to begin with.

--- client/src/warrior.js
+++ client/src/warrior.js
@@ -6,4 +6,13 @@ export default class Warrior extends Player {
     constructor(id, name) {
         super(id, name, Entities.WARRIOR);
     }
+
+    removeTarget() {
+        if (this.target) {
+            // Attacking mobs is handled separately for this client's player (i.e. the warrior).
+            // The client's player is not added as an attacker of another character.
+
+            this.target = null;
+        }
+    }
 }
