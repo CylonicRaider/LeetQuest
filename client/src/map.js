CylonicRaider/LeetQuest

commit d222273648809723a8118c1d611947b8d0f316c4

Modernize code
+ Restores the code to a working state (albeit with some issues, see
  below)
+ Refactor to ES6 module syntax
+ Use of webpack for client bundling
+ Replace underscore with lodash-es
+ Replace sanitizer with sanitze-html and he

TODO:
- Resolve cyclic dependency between Area and Mob in server code,
  the part using Mob in the Area code is commented out, preventing
  proper mob handling in Areas
- Fix issue where the client code tries to remove the attacker
  (which in this case is the player) of a Mob, but the Mob had already
  been killed before this, causing an error.
- Fix metrics code as it is currently broken and commented out at the
  import locations

client/src/map.js
@@ -1,324 +1,334 @@
-define(["jquery", "area"], function ($, Area) {
-    var Map = Class.extend({
-        init: function (loadMultiTilesheets, game) {
-            this.game = game;
-            this.data = [];
-            this.isLoaded = false;
-            this.tilesetsLoaded = false;
-            this.mapLoaded = false;
-            this.loadMultiTilesheets = loadMultiTilesheets;
+import find from "lodash-es/find.js";
+import forEach from "lodash-es/forEach.js";
+import includes from "lodash-es/includes.js";
 
-            var useWorker = !(
-                this.game.renderer.mobile || this.game.renderer.tablet
-            );
+import { Orientations } from "../../shared/js/gametypes.js";
 
-            this._loadMap(useWorker);
-            this._initTilesets();
-        },
+import Area from "./area.js";
+import log from "./lib/log.js";
+import { isInt } from "./util.js";
 
-        _checkReady: function () {
-            if (this.tilesetsLoaded && this.mapLoaded) {
-                this.isLoaded = true;
-                if (this.ready_func) {
-                    this.ready_func();
-                }
+export default class WorldMap {
+    constructor(loadMultiTilesheets, game) {
+        this.game = game;
+        this.data = [];
+        this.isLoaded = false;
+        this.tilesetsLoaded = false;
+        this.mapLoaded = false;
+        this.loadMultiTilesheets = loadMultiTilesheets;
+
+        var useWorker = !(
+            this.game.renderer.mobile || this.game.renderer.tablet
+        );
+
+        this._loadMap(useWorker);
+        this._initTilesets();
+    }
+
+    _checkReady() {
+        if (this.tilesetsLoaded && this.mapLoaded) {
+            this.isLoaded = true;
+            if (this.ready_func) {
+                this.ready_func();
             }
-        },
+        }
+    }
 
-        _loadMap: function (useWorker) {
-            var self = this,
-                filepath = "maps/world_client.json";
+    _loadMap(useWorker) {
+        // TODO: check if this works
+        // var filepath = "maps/world_client.json";
 
-            if (useWorker) {
-                log.info("Loading map with web worker.");
-                var worker = new Worker("js/mapworker.js");
-                worker.postMessage(1);
+        if (useWorker) {
+            log.info("Loading map with web worker.");
+            var worker = new Worker(new URL("./mapworker.js", import.meta.url));
+            worker.postMessage(1);
 
-                worker.onmessage = function (event) {
-                    var map = event.data;
-                    self._initMap(map);
-                    self.grid = map.grid;
-                    self.plateauGrid = map.plateauGrid;
-                    self.mapLoaded = true;
-                    self._checkReady();
-                };
+            worker.onmessage = (event) => {
+                var map = event.data;
+                this._initMap(map);
+                this.grid = map.grid;
+                this.plateauGrid = map.plateauGrid;
+                this.mapLoaded = true;
+                this._checkReady();
+            };
+        } else {
+            log.info("Loading map via Ajax.");
+            import("../maps/world_client.json").then((data) => {
+                this._initMap(data);
+                this._generateCollisionGrid();
+                this._generatePlateauGrid();
+                this.mapLoaded = true;
+                this._checkReady();
+            });
+            // $.get(
+            //     filepath,
+            //     (data) => {
+            //         this._initMap(data);
+            //         this._generateCollisionGrid();
+            //         this._generatePlateauGrid();
+            //         this.mapLoaded = true;
+            //         this._checkReady();
+            //     },
+            //     "json",
+            // );
+        }
+    }
+
+    _initTilesets() {
+        var tileset1, tileset2, tileset3;
+
+        if (!this.loadMultiTilesheets) {
+            this.tilesetCount = 1;
+            tileset1 = this._loadTileset("img/1/tilesheet.png");
+        } else {
+            if (this.game.renderer.mobile || this.game.renderer.tablet) {
+                this.tilesetCount = 1;
+                tileset2 = this._loadTileset("img/2/tilesheet.png");
             } else {
-                log.info("Loading map via Ajax.");
-                $.get(
-                    filepath,
-                    function (data) {
-                        self._initMap(data);
-                        self._generateCollisionGrid();
-                        self._generatePlateauGrid();
-                        self.mapLoaded = true;
-                        self._checkReady();
-                    },
-                    "json",
+                this.tilesetCount = 2;
+                tileset2 = this._loadTileset("img/2/tilesheet.png");
+                tileset3 = this._loadTileset("img/3/tilesheet.png");
+            }
+        }
+
+        this.tilesets = [tileset1, tileset2, tileset3];
+    }
+
+    _initMap(map) {
+        this.width = map.width;
+        this.height = map.height;
+        this.tilesize = map.tilesize;
+        this.data = map.data;
+        this.blocking = map.blocking || [];
+        this.plateau = map.plateau || [];
+        this.musicAreas = map.musicAreas || [];
+        this.collisions = map.collisions;
+        this.high = map.high;
+        this.animated = map.animated;
+
+        this.doors = this._getDoors(map);
+        this.checkpoints = this._getCheckpoints(map);
+    }
+
+    _getDoors(map) {
+        let doors = {};
+
+        forEach(map.doors, (door) => {
+            let o;
+
+            switch (door.to) {
+                case "u":
+                    o = Orientations.UP;
+                    break;
+                case "d":
+                    o = Orientations.DOWN;
+                    break;
+                case "l":
+                    o = Orientations.LEFT;
+                    break;
+                case "r":
+                    o = Orientations.RIGHT;
+                    break;
+                default:
+                    o = Orientations.DOWN;
+            }
+
+            doors[this.GridPositionToTileIndex(door.x, door.y)] = {
+                x: door.tx,
+                y: door.ty,
+                orientation: o,
+                cameraX: door.tcx,
+                cameraY: door.tcy,
+                portal: door.p === 1,
+            };
+        });
+
+        return doors;
+    }
+
+    _loadTileset(filepath) {
+        const tileset = new Image();
+
+        tileset.src = filepath;
+
+        log.info("Loading tileset: " + filepath);
+
+        tileset.onload = () => {
+            if (tileset.width % this.tilesize > 0) {
+                throw Error(
+                    `Tileset size should be a multiple of ${this.tilesize}`,
                 );
             }
-        },
+            log.info("Map tileset loaded.");
 
-        _initTilesets: function () {
-            var tileset1, tileset2, tileset3;
+            this.tilesetCount -= 1;
+            if (this.tilesetCount === 0) {
+                log.debug("All map tilesets loaded.");
 
-            if (!this.loadMultiTilesheets) {
-                this.tilesetCount = 1;
-                tileset1 = this._loadTileset("img/1/tilesheet.png");
-            } else {
-                if (this.game.renderer.mobile || this.game.renderer.tablet) {
-                    this.tilesetCount = 1;
-                    tileset2 = this._loadTileset("img/2/tilesheet.png");
+                this.tilesetsLoaded = true;
+                this._checkReady();
+            }
+        };
+
+        return tileset;
+    }
+
+    ready(f) {
+        this.ready_func = f;
+    }
+
+    tileIndexToGridPosition(tileNum) {
+        var x = 0,
+            y = 0;
+
+        var getX = (num, w) => {
+            if (num == 0) {
+                return 0;
+            }
+            return num % w == 0 ? w - 1 : (num % w) - 1;
+        };
+
+        tileNum -= 1;
+        x = getX(tileNum + 1, this.width);
+        y = Math.floor(tileNum / this.width);
+
+        return { x: x, y: y };
+    }
+
+    GridPositionToTileIndex(x, y) {
+        return y * this.width + x + 1;
+    }
+
+    isColliding(x, y) {
+        if (this.isOutOfBounds(x, y) || !this.grid) {
+            return false;
+        }
+        return this.grid[y][x] === 1;
+    }
+
+    isPlateau(x, y) {
+        if (this.isOutOfBounds(x, y) || !this.plateauGrid) {
+            return false;
+        }
+        return this.plateauGrid[y][x] === 1;
+    }
+
+    _generateCollisionGrid() {
+        var tileIndex = 0;
+
+        this.grid = [];
+        for (var j, i = 0; i < this.height; i++) {
+            this.grid[i] = [];
+            for (j = 0; j < this.width; j++) {
+                this.grid[i][j] = 0;
+            }
+        }
+
+        forEach(this.collisions, (tileIndex) => {
+            var pos = this.tileIndexToGridPosition(tileIndex + 1);
+            this.grid[pos.y][pos.x] = 1;
+        });
+
+        forEach(this.blocking, (tileIndex) => {
+            var pos = this.tileIndexToGridPosition(tileIndex + 1);
+            if (this.grid[pos.y] !== undefined) {
+                this.grid[pos.y][pos.x] = 1;
+            }
+        });
+        log.info("Collision grid generated.");
+    }
+
+    _generatePlateauGrid() {
+        var tileIndex = 0;
+
+        this.plateauGrid = [];
+        for (var j, i = 0; i < this.height; i++) {
+            this.plateauGrid[i] = [];
+            for (j = 0; j < this.width; j++) {
+                if (includes(this.plateau, tileIndex)) {
+                    this.plateauGrid[i][j] = 1;
                 } else {
-                    this.tilesetCount = 2;
-                    tileset2 = this._loadTileset("img/2/tilesheet.png");
-                    tileset3 = this._loadTileset("img/3/tilesheet.png");
+                    this.plateauGrid[i][j] = 0;
                 }
+                tileIndex += 1;
             }
+        }
+        log.info("Plateau grid generated.");
+    }
 
-            this.tilesets = [tileset1, tileset2, tileset3];
-        },
+    /**
+     * Returns true if the given position is located within the dimensions of the map.
+     *
+     * @returns {boolean} Whether the position is out of bounds.
+     */
+    isOutOfBounds(x, y) {
+        return (
+            isInt(x) &&
+            isInt(y) &&
+            (x < 0 || x >= this.width || y < 0 || y >= this.height)
+        );
+    }
 
-        _initMap: function (map) {
-            this.width = map.width;
-            this.height = map.height;
-            this.tilesize = map.tilesize;
-            this.data = map.data;
-            this.blocking = map.blocking || [];
-            this.plateau = map.plateau || [];
-            this.musicAreas = map.musicAreas || [];
-            this.collisions = map.collisions;
-            this.high = map.high;
-            this.animated = map.animated;
+    /**
+     * Returns true if the given tile id is "high", i.e. above all entities.
+     * Used by the renderer to know which tiles to draw after all the entities
+     * have been drawn.
+     *
+     * @param {number} id The tile id in the tileset
+     * @see Renderer.drawHighTiles
+     */
+    isHighTile(id) {
+        return includes(this.high, id + 1);
+    }
 
-            this.doors = this._getDoors(map);
-            this.checkpoints = this._getCheckpoints(map);
-        },
+    /**
+     * Returns true if the tile is animated. Used by the renderer.
+     * @param {number} id The tile id in the tileset
+     */
+    isAnimatedTile(id) {
+        return id + 1 in this.animated;
+    }
 
-        _getDoors: function (map) {
-            var doors = {},
-                self = this;
+    /**
+     *
+     */
+    getTileAnimationLength(id) {
+        return this.animated[id + 1].l;
+    }
 
-            _.each(map.doors, function (door) {
-                var o;
+    /**
+     *
+     */
+    getTileAnimationDelay(id) {
+        var animProperties = this.animated[id + 1];
+        if (animProperties.d) {
+            return animProperties.d;
+        } else {
+            return 100;
+        }
+    }
 
-                switch (door.to) {
-                    case "u":
-                        o = Types.Orientations.UP;
-                        break;
-                    case "d":
-                        o = Types.Orientations.DOWN;
-                        break;
-                    case "l":
-                        o = Types.Orientations.LEFT;
-                        break;
-                    case "r":
-                        o = Types.Orientations.RIGHT;
-                        break;
-                    default:
-                        o = Types.Orientations.DOWN;
-                }
+    isDoor(x, y) {
+        return this.doors[this.GridPositionToTileIndex(x, y)] !== undefined;
+    }
 
-                doors[self.GridPositionToTileIndex(door.x, door.y)] = {
-                    x: door.tx,
-                    y: door.ty,
-                    orientation: o,
-                    cameraX: door.tcx,
-                    cameraY: door.tcy,
-                    portal: door.p === 1,
-                };
-            });
+    getDoorDestination(x, y) {
+        return this.doors[this.GridPositionToTileIndex(x, y)];
+    }
 
-            return doors;
-        },
+    _getCheckpoints(map) {
+        var checkpoints = [];
+        forEach(map.checkpoints, (cp) => {
+            var area = new Area(cp.x, cp.y, cp.w, cp.h);
+            area.id = cp.id;
+            checkpoints.push(area);
+        });
+        return checkpoints;
+    }
 
-        _loadTileset: function (filepath) {
-            var self = this;
-            var tileset = new Image();
-
-            tileset.src = filepath;
-
-            log.info("Loading tileset: " + filepath);
-
-            tileset.onload = function () {
-                if (tileset.width % self.tilesize > 0) {
-                    throw Error(
-                        "Tileset size should be a multiple of " + self.tilesize,
-                    );
-                }
-                log.info("Map tileset loaded.");
-
-                self.tilesetCount -= 1;
-                if (self.tilesetCount === 0) {
-                    log.debug("All map tilesets loaded.");
-
-                    self.tilesetsLoaded = true;
-                    self._checkReady();
-                }
-            };
-
-            return tileset;
-        },
-
-        ready: function (f) {
-            this.ready_func = f;
-        },
-
-        tileIndexToGridPosition: function (tileNum) {
-            var x = 0,
-                y = 0;
-
-            var getX = function (num, w) {
-                if (num == 0) {
-                    return 0;
-                }
-                return num % w == 0 ? w - 1 : (num % w) - 1;
-            };
-
-            tileNum -= 1;
-            x = getX(tileNum + 1, this.width);
-            y = Math.floor(tileNum / this.width);
-
-            return { x: x, y: y };
-        },
-
-        GridPositionToTileIndex: function (x, y) {
-            return y * this.width + x + 1;
-        },
-
-        isColliding: function (x, y) {
-            if (this.isOutOfBounds(x, y) || !this.grid) {
-                return false;
-            }
-            return this.grid[y][x] === 1;
-        },
-
-        isPlateau: function (x, y) {
-            if (this.isOutOfBounds(x, y) || !this.plateauGrid) {
-                return false;
-            }
-            return this.plateauGrid[y][x] === 1;
-        },
-
-        _generateCollisionGrid: function () {
-            var tileIndex = 0,
-                self = this;
-
-            this.grid = [];
-            for (var j, i = 0; i < this.height; i++) {
-                this.grid[i] = [];
-                for (j = 0; j < this.width; j++) {
-                    this.grid[i][j] = 0;
-                }
-            }
-
-            _.each(this.collisions, function (tileIndex) {
-                var pos = self.tileIndexToGridPosition(tileIndex + 1);
-                self.grid[pos.y][pos.x] = 1;
-            });
-
-            _.each(this.blocking, function (tileIndex) {
-                var pos = self.tileIndexToGridPosition(tileIndex + 1);
-                if (self.grid[pos.y] !== undefined) {
-                    self.grid[pos.y][pos.x] = 1;
-                }
-            });
-            log.info("Collision grid generated.");
-        },
-
-        _generatePlateauGrid: function () {
-            var tileIndex = 0;
-
-            this.plateauGrid = [];
-            for (var j, i = 0; i < this.height; i++) {
-                this.plateauGrid[i] = [];
-                for (j = 0; j < this.width; j++) {
-                    if (_.include(this.plateau, tileIndex)) {
-                        this.plateauGrid[i][j] = 1;
-                    } else {
-                        this.plateauGrid[i][j] = 0;
-                    }
-                    tileIndex += 1;
-                }
-            }
-            log.info("Plateau grid generated.");
-        },
-
-        /**
-         * Returns true if the given position is located within the dimensions of the map.
-         *
-         * @returns {Boolean} Whether the position is out of bounds.
-         */
-        isOutOfBounds: function (x, y) {
-            return (
-                isInt(x) &&
-                isInt(y) &&
-                (x < 0 || x >= this.width || y < 0 || y >= this.height)
-            );
-        },
-
-        /**
-         * Returns true if the given tile id is "high", i.e. above all entities.
-         * Used by the renderer to know which tiles to draw after all the entities
-         * have been drawn.
-         *
-         * @param {Number} id The tile id in the tileset
-         * @see Renderer.drawHighTiles
-         */
-        isHighTile: function (id) {
-            return _.indexOf(this.high, id + 1) >= 0;
-        },
-
-        /**
-         * Returns true if the tile is animated. Used by the renderer.
-         * @param {Number} id The tile id in the tileset
-         */
-        isAnimatedTile: function (id) {
-            return id + 1 in this.animated;
-        },
-
-        /**
-         *
-         */
-        getTileAnimationLength: function (id) {
-            return this.animated[id + 1].l;
-        },
-
-        /**
-         *
-         */
-        getTileAnimationDelay: function (id) {
-            var animProperties = this.animated[id + 1];
-            if (animProperties.d) {
-                return animProperties.d;
-            } else {
-                return 100;
-            }
-        },
-
-        isDoor: function (x, y) {
-            return this.doors[this.GridPositionToTileIndex(x, y)] !== undefined;
-        },
-
-        getDoorDestination: function (x, y) {
-            return this.doors[this.GridPositionToTileIndex(x, y)];
-        },
-
-        _getCheckpoints: function (map) {
-            var checkpoints = [];
-            _.each(map.checkpoints, function (cp) {
-                var area = new Area(cp.x, cp.y, cp.w, cp.h);
-                area.id = cp.id;
-                checkpoints.push(area);
-            });
-            return checkpoints;
-        },
-
-        getCurrentCheckpoint: function (entity) {
-            return _.detect(this.checkpoints, function (checkpoint) {
-                return checkpoint.contains(entity);
-            });
-        },
-    });
-
-    return Map;
-});
+    getCurrentCheckpoint(entity) {
+        return find(this.checkpoints, (checkpoint) =>
+            checkpoint.contains(entity),
+        );
+    }
+}

commit 38c12a48f319b44d6f214b743e69131746c2b0d9

Fix map loading bug and issues
Oh, hey, non-worker map loading has been broken all the time since
commit d222273648809723a8118c1d611947b8d0f316c4. The regression was
revealed in commit d1fae02b62dffc3a0435e242065f116e90163784.

This commit also fixes some particularly ugly grid building loops.

client/src/map.js
@@ -33,7 +33,7 @@ export default class WorldMap {
     _loadMap() {
         // TODO: Ensure the map is not just bundled into the client code.
         log.info("Loading map...");
-        import("../maps/world_client.json").then((data) => {
+        import("../maps/world_client.json").then(({ default: data }) => {
             this._initMap(data);
             this._generateCollisionGrid();
             this._generatePlateauGrid();
@@ -183,10 +183,10 @@ export default class WorldMap {
 
     _generateCollisionGrid() {
         this.grid = [];
-        for (var j, i = 0; i < this.height; i++) {
-            this.grid[i] = [];
-            for (j = 0; j < this.width; j++) {
-                this.grid[i][j] = 0;
+        for (let i = 0; i < this.height; i++) {
+            this.grid.push([]);
+            for (let j = 0; j < this.width; j++) {
+                this.grid[i].push(0);
             }
         }
 
@@ -197,9 +197,7 @@ export default class WorldMap {
 
         forEach(this.blocking, (tileIndex) => {
             var pos = this.tileIndexToGridPosition(tileIndex + 1);
-            if (this.grid[pos.y] !== undefined) {
-                this.grid[pos.y][pos.x] = 1;
-            }
+            this.grid[pos.y][pos.x] = 1;
         });
         log.info("Collision grid generated.");
     }
@@ -208,15 +206,15 @@ export default class WorldMap {
         var tileIndex = 0;
 
         this.plateauGrid = [];
-        for (var j, i = 0; i < this.height; i++) {
-            this.plateauGrid[i] = [];
-            for (j = 0; j < this.width; j++) {
+        for (let i = 0; i < this.height; i++) {
+            this.plateauGrid.push([]);
+            for (let j = 0; j < this.width; j++) {
                 if (includes(this.plateau, tileIndex)) {
-                    this.plateauGrid[i][j] = 1;
+                    this.plateauGrid[i].push(1);
                 } else {
-                    this.plateauGrid[i][j] = 0;
+                    this.plateauGrid[i].push(0);
                 }
-                tileIndex += 1;
+                tileIndex++;
             }
         }
         log.info("Plateau grid generated.");