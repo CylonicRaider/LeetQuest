a4b59d0d563d3ee9da1205be40d3b76b3d84651b
CylonicRaider <CylonicRaider@users.noreply.github.com>
2021-09-14 23:57:07 +0200

    Drop redundant TODO
    Yes, writing errors and warnings to a file is a form of logging.

--- server/src/log.js
+++ server/src/log.js
@@ -1,8 +1,6 @@
 import pino from "pino";
 import pinoHttp from "pino-http";
 
-// TODO: consider writing errors and warnings to a file for production
-
 const LOGGER = pino({ level: process.env.LOG_LEVEL || "info" });
 
 export default LOGGER;
