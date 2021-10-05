19a223dac9777a997e3ad70c558c57b36a13a224
CylonicRaider <CylonicRaider@users.noreply.github.com>
2021-10-05 20:27:02 +0200

    Add configuration files

--- bin/run-on-changed.js
+++ bin/run-on-changed.js
@@ -8,8 +8,12 @@ import minimist from "minimist";
 import path from "path";
 import process from "process";
 
-function singleToArray(value) {
-    return value == null ? [] : Array.isArray(value) ? value : [value];
+function argsToArray(...values) {
+    return [].concat(...values.filter((v) => v != null));
+}
+
+function mergeArraySets(...arrays) {
+    return [...new Set([].concat(...arrays))];
 }
 
 function waitOnRawEvent(object, event, callback = null) {
@@ -30,6 +34,27 @@ async function hashStream(input, algorithm = "sha256") {
     const hasher = crypto.createHash(algorithm).setEncoding("hex");
     input.pipe(hasher);
     return await waitOnRawEvent(hasher, "finish", () => hasher.read());
+}
+
+export async function loadConfigs(files) {
+    const result = [];
+    for (const filename of files) {
+        result.push(JSON.parse(await fs.readFile(filename, "utf-8")));
+    }
+    return result;
+}
+
+function mergeConfigs(...objects) {
+    const result = {};
+    for (const obj of objects) {
+        if (typeof obj !== "object" || Array.isArray(obj)) {
+            continue;
+        }
+        for (const [key, value] of Object.entries(obj)) {
+            result[key] = argsToArray(result[key], value);
+        }
+    }
+    return result;
 }
 
 export async function loadCache(location) {
@@ -112,15 +137,35 @@ export async function saveCache(location, state) {
 
 export default async function main(argv) {
     const args = minimist(argv);
-    const cacheFile = args.c;
-    const globalFiles = singleToArray(args.g);
-    const entryPoints = singleToArray(args.e);
-    const ignoreFiles = singleToArray(args.i);
-    const extensions = singleToArray(args.x);
+    const cliConfig = {
+        cache: argsToArray(args.state, args.s),
+        global: argsToArray(args.global, args.g),
+        paths: argsToArray(args.path, args.p),
+        ignore: argsToArray(args.ignore, args.i),
+        extensions: argsToArray(args.extension, args.x),
+    };
+    const configFiles = argsToArray(args.config, args.c);
     const cmdline = args._;
 
+    const fileConfigs = await loadConfigs(configFiles);
+    const config = mergeConfigs(...fileConfigs, cliConfig);
+
+    let cacheFile;
+    if (config.cache.length == 0) {
+        cacheFile = null;
+    } else if (config.cache.length == 1) {
+        cacheFile = config.cache[0];
+    } else {
+        throw new Error("More than one cache file specified");
+    }
+    const globalFiles = mergeArraySets(...configFiles, ...config.global);
+
     const state = await loadCache(cacheFile);
-    const rawFiles = await enumerateFiles(entryPoints, extensions, ignoreFiles);
+    const rawFiles = await enumerateFiles(
+        config.paths,
+        config.extensions,
+        config.ignore,
+    );
     pruneCache(rawFiles, state);
     const files = await filterFiles(rawFiles, globalFiles, state);
 
